3f535c66a5adc6a26dd7ccee4eac004632241560
furglitch <samsonger@proton.me>
2026-03-04 15:47:55 -0600

    feat: shaymin animation

---
+++ boards/shields/nice_oled/assets/shaymin.h
@@ -0,0 +1,138 @@
+#ifndef SHAYMIN_IMAGES_H
+#define SHAYMIN_IMAGES_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "lvgl.h"
+// -------- Image Descriptors Declarations --------
+LV_IMG_DECLARE(shaymin_00);
+LV_IMG_DECLARE(shaymin_01);
+LV_IMG_DECLARE(shaymin_02);
+LV_IMG_DECLARE(shaymin_03);
+LV_IMG_DECLARE(shaymin_04);
+LV_IMG_DECLARE(shaymin_05);
+LV_IMG_DECLARE(shaymin_06);
+LV_IMG_DECLARE(shaymin_07);
+LV_IMG_DECLARE(shaymin_08);
+LV_IMG_DECLARE(shaymin_09);
+LV_IMG_DECLARE(shaymin_10);
+LV_IMG_DECLARE(shaymin_11);
+LV_IMG_DECLARE(shaymin_12);
+LV_IMG_DECLARE(shaymin_13);
+LV_IMG_DECLARE(shaymin_14);
+LV_IMG_DECLARE(shaymin_15);
+LV_IMG_DECLARE(shaymin_16);
+LV_IMG_DECLARE(shaymin_17);
+LV_IMG_DECLARE(shaymin_18);
+LV_IMG_DECLARE(shaymin_19);
+LV_IMG_DECLARE(shaymin_20);
+LV_IMG_DECLARE(shaymin_21);
+LV_IMG_DECLARE(shaymin_22);
+LV_IMG_DECLARE(shaymin_23);
+LV_IMG_DECLARE(shaymin_24);
+LV_IMG_DECLARE(shaymin_25);
+LV_IMG_DECLARE(shaymin_26);
+LV_IMG_DECLARE(shaymin_27);
+LV_IMG_DECLARE(shaymin_28);
+LV_IMG_DECLARE(shaymin_29);
+LV_IMG_DECLARE(shaymin_30);
+LV_IMG_DECLARE(shaymin_31);
+LV_IMG_DECLARE(shaymin_32);
+LV_IMG_DECLARE(shaymin_33);
+LV_IMG_DECLARE(shaymin_34);
+LV_IMG_DECLARE(shaymin_35);
+LV_IMG_DECLARE(shaymin_36);
+LV_IMG_DECLARE(shaymin_37);
+LV_IMG_DECLARE(shaymin_38);
+LV_IMG_DECLARE(shaymin_39);
+LV_IMG_DECLARE(shaymin_40);
+LV_IMG_DECLARE(shaymin_41);
+LV_IMG_DECLARE(shaymin_42);
+LV_IMG_DECLARE(shaymin_43);
+LV_IMG_DECLARE(shaymin_44);
+LV_IMG_DECLARE(shaymin_45);
+LV_IMG_DECLARE(shaymin_46);
+LV_IMG_DECLARE(shaymin_47);
+LV_IMG_DECLARE(shaymin_48);
+LV_IMG_DECLARE(shaymin_49);
+LV_IMG_DECLARE(shaymin_50);
+LV_IMG_DECLARE(shaymin_51);
+LV_IMG_DECLARE(shaymin_52);
+LV_IMG_DECLARE(shaymin_53);
+LV_IMG_DECLARE(shaymin_54);
+LV_IMG_DECLARE(shaymin_55);
+LV_IMG_DECLARE(shaymin_56);
+LV_IMG_DECLARE(shaymin_57);
+LV_IMG_DECLARE(shaymin_58);
+LV_IMG_DECLARE(shaymin_59);
+
+// -------- Array of Pointers to Image Descriptors --------
+const lv_img_dsc_t *shaymin_images[60] = {
+    &shaymin_00,
+    &shaymin_01,
+    &shaymin_02,
+    &shaymin_03,
+    &shaymin_04,
+    &shaymin_05,
+    &shaymin_06,
+    &shaymin_07,
+    &shaymin_08,
+    &shaymin_09,
+    &shaymin_10,
+    &shaymin_11,
+    &shaymin_12,
+    &shaymin_13,
+    &shaymin_14,
+    &shaymin_15,
+    &shaymin_16,
+    &shaymin_17,
+    &shaymin_18,
+    &shaymin_19,
+    &shaymin_20,
+    &shaymin_21,
+    &shaymin_22,
+    &shaymin_23,
+    &shaymin_24,
+    &shaymin_25,
+    &shaymin_26,
+    &shaymin_27,
+    &shaymin_28,
+    &shaymin_29,
+    &shaymin_30,
+    &shaymin_31,
+    &shaymin_32,
+    &shaymin_33,
+    &shaymin_34,
+    &shaymin_35,
+    &shaymin_36,
+    &shaymin_37,
+    &shaymin_38,
+    &shaymin_39,
+    &shaymin_40,
+    &shaymin_41,
+    &shaymin_42,
+    &shaymin_43,
+    &shaymin_44,
+    &shaymin_45,
+    &shaymin_46,
+    &shaymin_47,
+    &shaymin_48,
+    &shaymin_49,
+    &shaymin_50,
+    &shaymin_51,
+    &shaymin_52,
+    &shaymin_53,
+    &shaymin_54,
+    &shaymin_55,
+    &shaymin_56,
+    &shaymin_57,
+    &shaymin_58,
+    &shaymin_59,
+};
+
+#define SHAYMIN_IMAGES_NUM_IMAGES 60
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* SHAYMIN_IMAGES_H */
